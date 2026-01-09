ba2ccc7719453724106c7e668b87e366bab0a181
Gizmo_ the_ great <DaddySun@me.com>
2026-01-09 01:16:04 +0100

    Refactor GPIO and tool management logic
    Refactor tool management functions and GPIO initialization. Consolidate tool enabling logic into a single function and improve NVS handling.

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -1,7 +1,12 @@
+
+#include "sdkconfig.h"
 #include <stdio.h>
-#include "driver\gpio.h"
-#include "freeRTOS\freeRTOS.h"
-#include "freeRTOS\task.h"
+#include "esp_log.h"
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "nvs_flash.h"
+#include "nvs.h"
 
 // Define ms delay
 #define DELAY(ms)           vTaskDelay(pdMS_TO_TICKS(ms))
@@ -19,7 +24,7 @@
 #define RELAIS_DISABLED     0
 
 // === LED PINS ===
-#define TOOL1_LED           GPIO_NUM_35
+#define TOOL1_LED           GPIO_NUM_23
 #define TOOL2_LED           GPIO_NUM_33
 #define TOOL3_LED           GPIO_NUM_26
 #define TOOL4_LED           GPIO_NUM_12
@@ -35,11 +40,26 @@
 
 #define SWITCH_ENABLED     0
 
-// Variable which saves active tool
-int active_tool = 0;
+#define NUM_TOOLS           4
+#define TOOL_SWITCH_PINS    {TOOL1_SWITCH, TOOL2_SWITCH, TOOL3_SWITCH, TOOL4_SWITCH}
 
-// Configures input and output pins
-void configure_gpio() {
+// === SWITCH STATUS VALUES ===
+#define SWITCH_ENABLED      0
+
+// === ACTIVE TOOL STATUS VALUED ===
+#define TOOLS_DISABLED      0
+#define TOOL_1              1
+#define TOOL_2              2
+#define TOOL_3              3
+#define TOOL_4              4
+
+// === NVS KEY NAMES ===
+#define NVS_NAMESPACE       "relay_storage"
+#define ACTIVE_TOOL_KEY     "active_tool"
+
+// Initializes input and output pins
+void init_gpio()
+{
     // Configure output pins
     gpio_config_t output_conf = {};
     output_conf.mode = GPIO_MODE_OUTPUT;
@@ -71,8 +91,36 @@ void configure_gpio() {
     ESP_ERROR_CHECK(gpio_config(&input_conf));
 }
 
+// Initializes nvs
+void init_nvs(nvs_handle_t *nvs_handle)
+{
+    // Initialize nvs
+    esp_err_t err = nvs_flash_init();
+
+    // Check for error
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        // Ersase  nvs
+        ESP_ERROR_CHECK(nvs_flash_erase());
+
+        // Recreate nvs
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+
+    // Open nvs handle
+    err = nvs_open(NVS_NAMESPACE, NVS_READWRITE, nvs_handle);
+
+    // Check for error
+    if (err != ESP_OK) {
+        ESP_LOGE("NVS", "Failed to open NVS handle!");
+        return;
+    }
+}
+
 // Sets everything to disabled
-void disable_all() {
+void disable_all()
+{
     // Disable all relais
     gpio_set_level(POWER_RELAIS, RELAIS_DISABLED);
     gpio_set_level(SENSOR_RELAIS, RELAIS_DISABLED);
@@ -88,186 +136,135 @@ void disable_all() {
     gpio_set_level(TOOL4_LED, LED_DISABLED);
 }
 
-// Enables tool 1 (t210)
-void set_tool_1() {
-    // Check if tool is aready active
-    if (active_tool == 1) {
-        // Debounce
+// General set_tool function
+void set_tool(uint8_t *active_tool, uint8_t tool_number)
+{
+    // Debounce
+    if (*active_tool == tool_number) {
         DELAY(200);
         return;
     }
 
-    // Disable everything
+    // Disable all tools and relays
     disable_all();
 
-    // Enable required relais
-    // T210, small soldering pen
-    gpio_set_level(SENSOR_RELAIS, RELAIS_ENABLED);
-    gpio_set_level(TOOL1_RELAIS, RELAIS_ENABLED);
-
-    // Wait for JBC
-    DELAY(300);
-
-    // Enable power
-    gpio_set_level(POWER_RELAIS, RELAIS_ENABLED);
-  
-    // Enable led
-    gpio_set_level(TOOL1_LED, LED_ENABLED);
-  
-    // Set active tool
-    active_tool = 1;
-}
-
-// Enables tool 2
-void set_tool_2() {
-    // Check if tool is aready active
-    if (active_tool == 2) {
-        // Debounce
-        DELAY(200);
-        return;
+    // Set the tool's relay and LED based on the tool number
+    switch (tool_number)
+    {
+        case TOOLS_DISABLED:
+            // Do nothing
+            *active_tool = TOOLS_DISABLED;
+            return;
+        case TOOL_1:
+            gpio_set_level(SENSOR_RELAIS, RELAIS_ENABLED);
+            gpio_set_level(TOOL1_RELAIS, RELAIS_ENABLED);
+            gpio_set_level(TOOL1_LED, LED_ENABLED);
+            break;
+        case TOOL_2:
+            gpio_set_level(TOOL2_RELAIS, RELAIS_ENABLED);
+            gpio_set_level(TOOL2_LED, LED_ENABLED);
+            break;
+        case TOOL_3:
+            gpio_set_level(TOOL3_RELAIS, RELAIS_ENABLED);
+            gpio_set_level(TOOL3_LED, LED_ENABLED);
+            break;
+        case TOOL_4:
+            gpio_set_level(TOOL4_RELAIS, RELAIS_ENABLED);
+            gpio_set_level(TOOL4_LED, LED_ENABLED);
+            break;
+        default:
+            ESP_LOGE("set_tool", "Invalid tool number: %d", tool_number);
+            return;
     }
-  
-    // Disable everything
-    disable_all();
-  
-    // Enable required relais
-    gpio_set_level(TOOL2_RELAIS, RELAIS_ENABLED);
-  
-    // Wait for JBC
-    DELAY(300);
-  
-    // Enable power
+
+    // Enable the power relay
     gpio_set_level(POWER_RELAIS, RELAIS_ENABLED);
-  
-    // Enable led
-    gpio_set_level(TOOL2_LED, LED_ENABLED);
-  
-    // Set active tool
-    active_tool = 2;
+
+    // Save the active tool to NVS
+    *active_tool = tool_number;
 }
 
-// Enables tool 3
-void set_tool_3() {
-    // Check if tool is aready active
-    if (active_tool == 3) {
-        // Debounce
-        DELAY(200);
-        return;
+// Loads last tool state
+void nvs_load_tool_state(nvs_handle_t nvs_handle, uint8_t *active_tool)
+{
+    uint8_t prev_tool_state = TOOLS_DISABLED;
+
+    // Get relay state from last shutdown to restore
+    esp_err_t err = nvs_get_u8(nvs_handle, ACTIVE_TOOL_KEY, &prev_tool_state);
+
+    if (err == ESP_OK)
+    {
+        ESP_LOGI("NVS", "Restored active tool: %d", active_tool);
+    } else
+    {
+        ESP_LOGE("NVS", "Failed to load active tool!");
     }
-  
-    // Disable everything
-    disable_all();
-  
-    // Enable required relais
-    gpio_set_level(TOOL3_RELAIS, RELAIS_ENABLED);
-  
-    // Wait for JBC
-    DELAY(300);
-  
-    // Enable power
-    gpio_set_level(POWER_RELAIS, RELAIS_ENABLED);
-  
-    // Enable led
-    gpio_set_level(TOOL3_LED, LED_ENABLED);
-  
-    // Set active tool
-    active_tool = 3;
+
+    // Restore relay and LED state based on the loaded tool
+    set_tool(active_tool, prev_tool_state);
 }
 
-// Enables tool 4
-void set_tool_4() {
-    // Check if tool is aready active
-    if (active_tool == 4) {
-        // Debounce
-        DELAY(200);
-        return;
+// Saves tool state
+void nvs_save_tool_state(nvs_handle_t nvs_handle, uint8_t tool_state)
+{
+    esp_err_t err = nvs_set_u8(nvs_handle, ACTIVE_TOOL_KEY, tool_state);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE("NVS", "Failed to save active tool!");
     }
-  
-    // Disable everything
-    disable_all();
-  
-    // Enable required relais
-    gpio_set_level(TOOL4_RELAIS, RELAIS_ENABLED);
-  
-    // Wait for JBC
-    DELAY(300);
-  
-    // Enable power
-    gpio_set_level(POWER_RELAIS, RELAIS_ENABLED);
-  
-    // Enable led
-    gpio_set_level(TOOL4_LED, LED_ENABLED);
-  
-    // Set active tool
-    active_tool = 4;
+
+    // Write changes to storage
+    err = nvs_commit(nvs_handle);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE("NVS", "Failed to commit NVS!");
+    }
 }
 
+// Main application
 void app_main(void)
 {
-    // Configure digital I/O PINs
-    configure_gpio();
+    // Create required variables
+    gpio_num_t tool_switch_pins[NUM_TOOLS] = TOOL_SWITCH_PINS;
+    nvs_handle_t nvs_handle = 0;
+    uint8_t active_tool = TOOLS_DISABLED;
+
+    // Initialize GPIOs
+    init_gpio();
 
     // Disable periphery
     disable_all();
 
+    // Initialize nvs and restore last tool state
+    init_nvs(&nvs_handle);
+    nvs_load_tool_state(nvs_handle, &active_tool);
+
     while(1)
     {
-        // Check if button was pressed
-        if (gpio_get_level(TOOL1_SWITCH) == SWITCH_ENABLED) {
-            // Enable tool 1
-            set_tool_1();
+        // Loop through every tool input button
+        for (int i = 0; i < NUM_TOOLS; i++)
+        {
+            // Check if the current tool switch was pressed
+            if (gpio_get_level(tool_switch_pins[i]) == SWITCH_ENABLED)
+            {
+                // Enable the tool based on the loop index (i + 1 for tool numbers)
+                set_tool(&active_tool, i + 1);
 
-            // Wait debounce time
-            DEBOUNCE;
+                // Save tool state to nvs
+                nvs_save_tool_state(nvs_handle, active_tool);
 
-            // Wait for button to be released
-            while(gpio_get_level(TOOL1_SWITCH) == SWITCH_ENABLED) {
                 // Wait debounce time
                 DEBOUNCE;
+
+                // Wait for the button to be released
+                while (gpio_get_level(tool_switch_pins[i]) == SWITCH_ENABLED)
+                {
+                    // Wait debounce time
+                    DEBOUNCE;
+                }
             }
         }
 
-        // Check if button was pressed
-        if (gpio_get_level(TOOL2_SWITCH) == SWITCH_ENABLED) {
-            // Enable tool 2
-            set_tool_2();
-
-            // Wait debounce time
-            DEBOUNCE;
-
-            // Wait for button to be released
-            while(gpio_get_level(TOOL2_SWITCH) == SWITCH_ENABLED) {
-                // Wait debounce time
-                DEBOUNCE;
-            }
-        }
-
-        // Check if button was pressed
-        if (gpio_get_level(TOOL3_SWITCH) == SWITCH_ENABLED) {
-            // Enable tool 3
-            set_tool_3();
-
-            // Wait debounce time
-            DEBOUNCE;
-
-            // Wait for button to be released
-            while(gpio_get_level(TOOL3_SWITCH) == SWITCH_ENABLED) {
-                // Wait debounce time
-                DEBOUNCE;
-            }
-        }
-
-        // Check if button was pressed
-        if (gpio_get_level(TOOL4_SWITCH) == SWITCH_ENABLED) {
-            // Enable tool 4
-            set_tool_4();
-
-            // Wait for button to be released
-            while(gpio_get_level(TOOL4_SWITCH) == SWITCH_ENABLED) {
-                // Wait debounce time
-                DEBOUNCE;
-            }
-        }
         // Let IDLE0 task reset watchdog
         vTaskDelay(1);
     }
